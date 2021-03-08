d681850f1c15fd207fa07e0379de386dc00902f6
marin73tomas <you@example.com>
2021-03-08 13:38:42 -0400

    fixed display issue

--- src/js/views/Login.js
+++ src/js/views/Login.js
@@ -5,47 +5,45 @@ import LoginForm from "../components/LoginForm";
 import { AppContext } from "../store/AppContext";
 import { useHistory } from "react-router-dom";
 
-const Login = props => {
-	const history = useHistory();
-	const { store, actions } = useContext(AppContext);
-	useEffect(() => {
-		if (store.authAlert.title) {
-			console.log("qeueing dismissal");
-			setTimeout(() => {
-				actions.dismissAuthAlert();
-			}, 5000);
-		}
-		return () => {
-			// cleanup
-		};
-	}, [store.authAlert]); // only on first load...
-	return (
-		<Container fluid="true" className="home-bg-image h-100">
-			<Container className="p-0 mb-5">
-				{store.authAlert.title && (
-					<Alert
-						dismissible
-						className="mt-5"
-						variant={store.authAlert.variant}
-						onClose={e => {
-							actions.dismissAuthAlert();
-						}}
-					>
-						{store.authAlert.title}
-					</Alert>
-				)}
+const Login = (props) => {
+  const history = useHistory();
+  const { store, actions } = useContext(AppContext);
+  useEffect(() => {
+    if (store.authAlert.title) {
+      console.log("qeueing dismissal");
+      setTimeout(() => {
+        actions.dismissAuthAlert();
+      }, 5000);
+    }
+    return () => {
+      // cleanup
+    };
+  }, [store.authAlert]); // only on first load...
+  return (
+    <Container fluid="true" className="home-bg-image h-100">
+      <Container className="p-0 mb-5">
+        {store.authAlert.title && (
+          <Alert
+            dismissible
+            className="mt-5"
+            variant={store.authAlert.variant}
+            onClose={(e) => {
+              actions.dismissAuthAlert();
+            }}
+          >
+            {store.authAlert.title}
+          </Alert>
+        )}
 
-				<Jumbotron className="mt-4 mt-lg-5 home-jumbotron px-4 px-md-5">
-					<h1 className="display-4 text-center text-md-left">
-						{"Welcome, tracker!"}
-					</h1>
-					<LoginForm
-						goRegisterHandler={e => history.push("/register")}
-					/>
-				</Jumbotron>
-			</Container>
-		</Container>
-	);
+        <Jumbotron className="mt-4 mt-lg-5 home-jumbotron px-4 px-md-5">
+          <h1 className="display-4 text-center text-md-left">
+            {"Welcome, tracker!"}
+          </h1>
+          <LoginForm goRegisterHandler={(e) => history.push("/register")} />
+        </Jumbotron>
+      </Container>
+    </Container>
+  );
 };
 
 export default Login;
